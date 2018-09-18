9303ba1b54612cd5944199c2ac00babc6c6fbeb0
Richard Armuelles <samahel021@gmail.com>
2018-09-18 16:55:26 -0500

    update to new express version

--- project_code/Emotional_Maps/app.js
+++ project_code/Emotional_Maps/app.js
@@ -1,56 +1,42 @@
-(function() {
-  var app, compile, express, io, jade, nib, num_tweets, path, routes, server, stylus, twit, twitter, user;
+  var express = require("express");
+  var path = require("path");
+  var favicon = require('serve-favicon');
+  var logger = require('morgan');
+  var cookieParser = require('cookie-parser');
+  var bodyParser = require('body-parser');
+  var jade = require("jade");
+  var stylus = require("stylus");
+  var nib = require("nib");
+  var twitter = require("ntwitter");
 
-  express = require("express");
+  var index = require('./routes/index');
+  var users = require('./routes/users');
 
-  jade = require("jade");
-
-  stylus = require("stylus");
-
-  nib = require("nib");
-
-  twitter = require("ntwitter");
-
-  routes = require("./routes");
-
-  user = require("./routes/user");
-
-  app = express();
-
-  path = require("path");
-
-  server = require("http").createServer(app);
-
-  io = require("socket.io").listen(server);
+  var app = express();
+  server = require('http').Server(app);
+  io = require("socket.io")(server);
 
   num_tweets = 0;
 
   twit = new twitter({
-    consumer_key: 'C7xh0gGCm3btcMLwJI0A',
-    consumer_secret: 'ZGRR2hTOpwHH4J017RPZYPnqY4Np9gStTHSOYKHw',
-    access_token_key: '305235468-lRZ5wxlpjARUuLYfyXUerCrkM1dWZLQ1fkEfHKeg',
-    access_token_secret: 'YdDW69LKe1JnyY1uCPLjPuR3IifFXHZTO0RI7Q0bNw6MR'
+    consumer_key: '9Z2kW3H7ilTWnd8rPPwQcI3KZ',
+    consumer_secret: 'b5NQxSfVy2R21pUZnfDBLR8K0DJWytzIp2qoObtBE3jj70e4Zt',
+    access_token_key: '305235468-b6eR0qxeuboXlCM9x3BYKHoCY91qx7fRhehnXaAu',
+    access_token_secret: 'F0ZK5A7NkEHNUgT9106cT7qTtuDQ9mFxdspXzGZ5Tyutf'
   });
-    
-    io.configure('production', function(){
-        console.log("set config for production");
-        io.enable('browser client minification');  // send minified client
-        io.enable('browser client etag');          // apply etag caching logic based on version number
-        io.enable('browser client gzip');          // gzip the file
-        io.set('log level', 1);                    // reduce logging
-        io.set('transports', [                     // enable all transports (optional if you want flashsocket)
-            'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'
-        ]);
-      });
 
-  io.sockets.on("connection", function(socket) {
+  io.on("connection", function(socket) {
+    console.log("cliente conectado")
     twit.stream("statuses/filter", {
-      track: ["#emotionalmaps","emotionalmaps","excitement","excited","happiness","happy","serenity","serene","calm","calmed","alertness","alert","anticipation","anticipate","anticipated","interest","interesting","anger","angry","annoyance","annoyed","annoying","abhorrence","pain","painful","boredom","boring","sadness","sad","pensive","thoughtful","guess","amaze","amazed","amazing","surprised","surprising","surprise","distracted","distraction","distract","terrified","terrifying","fear","fearing","feared","apprehension","apprehensive","admiration","admire","admiring","confident","confide","trust","acceptance","accept","accepting","hasle","discomfort","hate"]
+      track: ["#emotionalmaps","emotionalmaps","excitement","excited","happiness","happy","serenity","serene","calm","calmed","alertness","alert","anticipation","anticipate",
+      "anticipated","interest","interesting","anger","angry","annoyance","annoyed","annoying","abhorrence","pain","painful","boredom","boring","sadness","sad","pensive",
+      "thoughtful","guess","amaze","amazed","amazing","surprised","surprising","surprise","distracted","distraction","distract","terrified","terrifying","fear","fearing",
+      "feared","apprehension","apprehensive","admiration","admire","admiring","confident","confide","trust","acceptance","accept","accepting","hasle","discomfort","hate"]
     }, function(stream) {
       stream.on("data", function(data) {
         if (data.geo !== null && data.lang == 'en') {
-            console.log('Posted near :' + data.geo.coordinates[0] + ' ' + data.geo.coordinates[1] + ' Tweet: ' + data.text);
-            io.sockets.emit("tweet", data);
+          console.log('Posted near :' + data.geo.coordinates[0] + ' ' + data.geo.coordinates[1] + ' Tweet: ' + data.text);
+          socket.emit("tweet", data);    
         }
       });
       return stream.on("error", function(data) {
@@ -66,47 +52,34 @@
     return stylus(str).set("filename", path).use(nib());
   };
 
-  app.set("port", process.env.PORT || 8000);
-
-  app.set("views", path.join(__dirname, "views"));
+  app.set('views', path.join(__dirname, 'views'));
+  app.use('/client', express.static(__dirname + '/node_modules'));
 
   app.set("view engine", "jade");
 
-  app.use(express.favicon());
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'images/icons/favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use(express.logger("dev"));
+  app.use('/', index);
+  app.use('/users', users);
 
-  app.use(express.bodyParser());
-
-  app.use(express.methodOverride());
-
-  app.use(app.router);
 
   app.use(stylus.middleware({
     src: __dirname + '/public',
     compile: compile
   }));
 
-  app.use(express["static"](path.join(__dirname, "public")));
-
   if ("production" === app.get("env")) {
     app.use(express.errorHandler());
   }
 
-  app.get("/", routes.index);
 
-  app.get("/users", user.list);
-
-  server.listen(app.get("port"), function() {
-    if (process.getuid() === 0) {
-      require("fs").stat(__filename, function(err, stats) {
-        if (err) {
-          return console.error(err);
-        }
-        return process.setuid(stats.uid);
-      });
-    }
-    return console.log("Express server listening on port " + app.get("port"));
-  });
-
-}).call(this);
+module.exports = {
+  app: app,
+  server: server
+};
--- project_code/Emotional_Maps/public/javascripts/io-connection.js
+++ project_code/Emotional_Maps/public/javascripts/io-connection.js
@@ -1,5 +1,5 @@
 $(function(){
-    
+
     var socket;
     var num_tweets = 0;
     var counter_excitement = 0;
@@ -26,18 +26,18 @@ $(function(){
     var counter_admiration = 0;
     var counter_confident = 0;
     var counter_acceptance = 0;
-    
-    socket = io.connect('/');
-    
+
+    var socket = io.connect();
+
     socket.on('tweet', function (data){
         var lat = data.geo.coordinates[0];
         var lng = data.geo.coordinates[1];
-        
+
         var name = data.user.name;
         var screen_name = data.user.screen_name;
         var tweetText = data.text;
         var displayPicture = data.user.profile_image_url;
-        
+
         var excitement = document.getElementById("strong_excitement");
         var happiness = document.getElementById("strong_happiness");
         var serenity = document.getElementById("strong_serenity");
@@ -62,273 +62,273 @@ $(function(){
         var admiration = document.getElementById("strong_admiration");
         var confident = document.getElementById("strong_confident");
         var acceptance = document.getElementById("strong_acceptance");
-                
+
         var myLatLng = new google.maps.LatLng(lat,lng);
         var marker = new google.maps.Marker({
             position: myLatLng,
             map: map
         });
-            
+
         if (/excitement/.test(tweetText) || /excited/.test(tweetText) || /Excitement/.test(tweetText) || /Excited/.test(tweetText) || /EXCITEMENT/.test(tweetText) || /EXCITED/.test(tweetText)){
             var iconFile = '/images/markers/excitement.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_excitement.getPath();
             path.push(myLatLng);
-            
+
             counter_excitement++;
             excitement.innerHTML = counter_excitement;
         }
-            
+
         if (/happiness/.test(tweetText) || /happy/.test(tweetText) || /Happy/.test(tweetText) || /Happiness/.test(tweetText) || /HAPPY/.test(tweetText) || /HAPPINESS/.test(tweetText)){
             var iconFile = '/images/markers/happiness.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_happiness.getPath();
             path.push(myLatLng);
-            
+
             counter_happiness++;
             happiness.innerHTML = counter_happiness;
         }
-            
+
         if (/serenity/.test(tweetText) || /serene/.test(tweetText) || /calm/.test(tweetText) || /calmed/.test(tweetText) || /Serenity/.test(tweetText) || /Serene/.test(tweetText) || /Calm/.test(tweetText) || /Calmed/.test(tweetText) || /SERENITY/.test(tweetText) || /SERENE/.test(tweetText) || /CALM/.test(tweetText) || /CALMED/.test(tweetText)){
             var iconFile = '/images/markers/serenity.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_serenity.getPath();
             path.push(myLatLng);
-            
+
             counter_serenity++;
             serenity.innerHTML = counter_serenity;
-        }  
-            
+        }
+
         if (/alertness/.test(tweetText) || /alert/.test(tweetText) || /Alertness/.test(tweetText) || /Alert/.test(tweetText) || /ALERTNESS/.test(tweetText) || /ALERT/.test(tweetText)){
             var iconFile = '/images/markers/alertness.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_alertness.getPath();
             path.push(myLatLng);
-            
+
             counter_alertness++;
             alertness.innerHTML = counter_alertness;
         }
-            
+
         if (/anticipation/.test(tweetText) || /anticipate/.test(tweetText) || /anticipated/.test(tweetText) || /Anticipation/.test(tweetText) || /Anticipate/.test(tweetText) || /Anticipated/.test(tweetText) || /ANTICIPATION/.test(tweetText) || /ANTICIPATE/.test(tweetText) || /ANTICIPATED/.test(tweetText)){
             var iconFile = '/images/markers/anticipation.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_anticipation.getPath();
             path.push(myLatLng);
-            
+
             counter_anticipation++;
             anticipation.innerHTML = counter_anticipation;
         }
-            
+
         if (/interest/.test(tweetText) || /interesting/.test(tweetText)){
             var iconFile = '/images/markers/interest.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_interest.getPath();
             path.push(myLatLng);
-            
+
             counter_interest++;
             interest.innerHTML = counter_interest;
         }
-        
+
         if (/anger/.test(tweetText) || /angry/.test(tweetText) || /Anger/.test(tweetText) || /Angry/.test(tweetText) || /ANGER/.test(tweetText) || /ANGRY/.test(tweetText)){
             var iconFile = '/images/markers/anger.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_anger.getPath();
             path.push(myLatLng);
-            
+
             counter_anger++;
             anger.innerHTML = counter_anger;
         }
-        
+
         if (/annoyance/.test(tweetText) || /annoyed/.test(tweetText) || /annoying/.test(tweetText) || /Annoyance/.test(tweetText) || /Annoyed/.test(tweetText) || /Annoying/.test(tweetText) || /ANNOYANCE/.test(tweetText) || /ANNOYED/.test(tweetText) || /ANNOYING/.test(tweetText)){
             var iconFile = '/images/markers/annoyance.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_annoyance.getPath();
             path.push(myLatLng);
-            
+
             counter_annoyance++;
             annoyance.innerHTML = counter_annoyance;
         }
-        
+
         if (/hasle/.test(tweetText) || /Hasle/.test(tweetText) || /HASLE/.test(tweetText) || /DISCOMFORT/.test(tweetText) || /discomfort/.test(tweetText) || /Discomfort/.test(tweetText)){
             var iconFile = '/images/markers/hasle.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_hasle.getPath();
             path.push(myLatLng);
-            
+
             counter_hasle++;
             hasle.innerHTML = counter_hasle;
         }
-        
+
         if (/abhorrence/.test(tweetText) || /Abhorrence/.test(tweetText) || /ABHORRENCE/.test(tweetText) || /hate/.test(tweetText) || /Hate/.test(tweetText) || /HATE/.test(tweetText)){
             var iconFile = '/images/markers/abhorrence.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_abhorrence.getPath();
             path.push(myLatLng);
-            
+
             counter_abhorrence++;
             abhorrence.innerHTML = counter_abhorrence;
         }
-        
+
         if (/pain/.test(tweetText) || /painful/.test(tweetText) || /Pain/.test(tweetText) || /Painful/.test(tweetText) || /PAIN/.test(tweetText) || /PAINFUL/.test(tweetText)){
             var iconFile = '/images/markers/pain.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_pain.getPath();
             path.push(myLatLng);
-            
+
             counter_pain++;
             pain.innerHTML = counter_pain;
         }
-        
+
         if (/boredom/.test(tweetText) || /boring/.test(tweetText) || /Boredom/.test(tweetText) || /Boring/.test(tweetText) || /BOREDOM/.test(tweetText) || /BORING/.test(tweetText)){
             var iconFile = '/images/markers/boredom.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_boredom.getPath();
             path.push(myLatLng);
-            
+
             counter_boredom++;
             boredom.innerHTML = counter_boredom;
         }
-        
+
         if (/sadness/.test(tweetText) || /sad/.test(tweetText) || /Sadness/.test(tweetText) || /Sad/.test(tweetText) || /SADNESS/.test(tweetText) || /SAD/.test(tweetText)){
             var iconFile = '/images/markers/sadness.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_sadness.getPath();
             path.push(myLatLng);
-            
+
             counter_sadness++;
             sadness.innerHTML = counter_sadness;
         }
-        
+
         if (/pensive/.test(tweetText) || /thoughtful/.test(tweetText) || /guess/.test(tweetText) || /Pensive/.test(tweetText) || /Thoughtful/.test(tweetText) || /Guess/.test(tweetText) || /PENSIVE/.test(tweetText) || /THOUGHTFUL/.test(tweetText) || /GUESS/.test(tweetText)){
             var iconFile = '/images/markers/pensive.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_pensive.getPath();
             path.push(myLatLng);
-            
+
             counter_pensive++;
             pensive.innerHTML = counter_pensive;
         }
-        
+
         if (/amaze/.test(tweetText) || /amazed/.test(tweetText) || /amazing/.test(tweetText) || /Amaze/.test(tweetText) || /Amazed/.test(tweetText) || /Amazing/.test(tweetText) || /AMAZE/.test(tweetText) || /AMAZED/.test(tweetText) || /AMAZING/.test(tweetText)){
             var iconFile = '/images/markers/amazed.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_amazed.getPath();
             path.push(myLatLng);
-            
+
             counter_amazed++;
             amazed.innerHTML = counter_amazed;
         }
-        
+
         if (/surprised/.test(tweetText) || /surprising/.test(tweetText) || /surprise/.test(tweetText) || /Surprised/.test(tweetText) || /Surprise/.test(tweetText) || /surprising/.test(tweetText)  || /SURPRISED/.test(tweetText) || /SURPRISING/.test(tweetText) || /SURPRISE/.test(tweetText)){
             var iconFile = '/images/markers/surprised.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_surprised.getPath();
             path.push(myLatLng);
-            
+
             counter_surprised++;
             surprised.innerHTML = counter_surprised;
         }
-        
+
         if (/distracted/.test(tweetText) || /distraction/.test(tweetText) || /Distracted/.test(tweetText) || /Distraction/.test(tweetText) || /DISTRACTED/.test(tweetText) || /DISTRACTION/.test(tweetText) || /DISTRACT/.test(tweetText) || /Distract/.test(tweetText) || /distract/.test(tweetText)){
             var iconFile = '/images/markers/distracted.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_distracted.getPath();
             path.push(myLatLng);
-            
+
             counter_distracted++;
             distracted.innerHTML = counter_distracted;
         }
-        
+
         if (/terrified/.test(tweetText) || /Terrified/.test(tweetText) || /TERRIFIED/.test(tweetText) || /terrifying/.test(tweetText) || /Terrifying/.test(tweetText) || /TERRIFYING/.test(tweetText)){
             var iconFile = '/images/markers/terrified.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_terrified.getPath();
             path.push(myLatLng);
-            
+
             counter_terrified++;
             terrified.innerHTML = counter_terrified;
         }
-        
+
         if (/fear/.test(tweetText) || /Fear/.test(tweetText) || /FEAR/.test(tweetText) || /fearing/.test(tweetText) || /Fearing/.test(tweetText) || /FEARING/.test(tweetText) || /feared/.test(tweetText) || /Feared/.test(tweetText) || /FEARED/.test(tweetText)){
             var iconFile = '/images/markers/fear.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_fear.getPath();
             path.push(myLatLng);
-            
+
             counter_fear++;
             fear.innerHTML = counter_fear;
         }
-        
+
         if (/apprehension/.test(tweetText) || /apprehensive/.test(tweetText) || /Apprehension/.test(tweetText) || /Apprehensive/.test(tweetText) || /APPREHENSION/.test(tweetText) || /APPREHENVISE/.test(tweetText)){
             var iconFile = '/images/markers/apprehension.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_apprehension.getPath();
             path.push(myLatLng);
-            
+
             counter_apprehension++;
             apprehension.innerHTML = counter_apprehension;
         }
-        
+
         if (/admiration/.test(tweetText) || /admire/.test(tweetText) || /admiring/.test(tweetText) || /Admiration/.test(tweetText) || /Admire/.test(tweetText) || /Admiring/.test(tweetText) || /ADMIRATION/.test(tweetText) || /ADMIRE/.test(tweetText) || /ADMIRING/.test(tweetText)){
             var iconFile = '/images/markers/admiration.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_admiration.getPath();
             path.push(myLatLng);
-            
+
             counter_admiration++;
             admiration.innerHTML = counter_admiration;
         }
-        
+
         if (/confident/.test(tweetText) || /confide/.test(tweetText) || /trust/.test(tweetText) || /Confident/.test(tweetText) || /Confide/.test(tweetText) || /Trust/.test(tweetText) || /CONFIDENT/.test(tweetText) || /CONFIDE/.test(tweetText) || /TRUST/.test(tweetText)){
             var iconFile = '/images/markers/confident.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_confident.getPath();
             path.push(myLatLng);
-            
+
             counter_confident++;
             confident.innerHTML = counter_confident;
         }
-        
+
         if (/acceptance/.test(tweetText) || /accept/.test(tweetText) || /Acceptance/.test(tweetText) || /Accept/.test(tweetText) || /ACCEPTANCE/.test(tweetText) || /ACCEPT/.test(tweetText) || /accepting/.test(tweetText) || /Accepting/.test(tweetText) || /ACCEPTING/.test(tweetText)){
             var iconFile = '/images/markers/acceptance.png';
             marker.setIcon(iconFile)
-            
+
             var path = poly_acceptance.getPath();
             path.push(myLatLng);
-            
+
             counter_acceptance++;
             acceptance.innerHTML = counter_acceptance;
         }
-        
+
 
         //crea un infowindow con el contenido del tweet
         var contentString = "<img class='displaypic' src='" + displayPicture + "'><div id='tweet-div'><strong> @" + screen_name + "</strong>: " + "<span class='tweetText'>" + tweetText + "</span></div>";
-        
+
         tw = document.getElementById("tweets");
         tw.innerHTML = "<div class='tweet'><div class='message'>" + contentString + '</div></div>';
-        
+
         //mueve el mapa para enfocar al proximo tweet
         var newFocus = new google.maps.LatLng(lat,lng);
         map.setCenter(newFocus);
